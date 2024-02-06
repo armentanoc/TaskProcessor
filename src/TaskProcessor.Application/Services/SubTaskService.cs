2bfe96ec43e1bb91a6ef7bcee1d2f04bdf191f7a
Carolina Armentano <armentanocarolina@gmail.com>
2024-02-06 17:47:39 -0300

    chore: removing unnecessary lines of code.

--- src/TaskProcessor.Application/Services/SubTaskService.cs
+++ src/TaskProcessor.Application/Services/SubTaskService.cs
@@ -44,50 +44,5 @@ namespace TaskProcessor.Application.Services
                 Console.WriteLine(ex.Message, ex.StackTrace);
             }
         }
-            public Task Start(int idTask)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task Pause(int idTask)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task Stop(int idTask)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task Resume(int idTask)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task Cancel(int idTask)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<TaskEntity> Create()
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<TaskEntity> Get(int idTarefa)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<IEnumerable<TaskEntity>> GetActiveTasks()
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<IEnumerable<TaskEntity>> GetInactiveTasks()
-        {
-            throw new NotImplementedException();
-        }
-
     }
 }
